17332a0cc411dc4204909d31d74a7c846a4dd24e
bergad37 <bergad37@gmail.com>
2023-03-19 11:24:05 +0200

    Added the loader to the landing page

--- Project1/js/AllBlogTable.js
+++ Project1/js/AllBlogTable.js
@@ -48,7 +48,16 @@ fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs')
             author.classList.add("light-text", "justify-text", "list", "p-1");
             body.appendChild(author);
 
-            const commentContainer = document.createElement('div')
+            const comments = document.createElement("ul");
+            comments.classList.add("light-text", "justify-text", "list", "p-1");
+            body.appendChild(comments);
+
+
+
+
+            const commentContainer = document.createElement('form');
+            commentContainer.classList.add("p-1");
+
             const commentBox = document.createElement("textarea");
             commentBox.classList.add("text-area");
             const commentBtn = document.createElement("button")
@@ -56,8 +65,13 @@ fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs')
             commentContainer.appendChild(commentBox);
             commentContainer.appendChild(commentBtn);
 
+            commentBtn.addEventListener('click', (e) => {
+                const newComment = document.createElement("li");
+                newComment.textContent = commentBox.value;
+                comments.appendChild(newComment);
+                newComment.value = "";
 
-
+            })
             const action = document.createElement("div");
             action.classList.add("title");
             const deleteButton = document.createElement("div");
@@ -70,7 +84,6 @@ fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs')
             update.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>'
             update.classList.add('icons');
             action.appendChild(update);
-
             //set the values
             imagesource.src = result.imageUrl;
             postDate.textContent = result.postedAt;
@@ -84,11 +97,12 @@ fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs')
 
             row.appendChild(image);
             row.appendChild(body);
-            row.appendChild(commentContainer);
             row.appendChild(action);
 
 
             blogTable.appendChild(row);
+            blogTable.appendChild(commentContainer);
+
             //Deleting a blog
             deleteButton.addEventListener("click", function() {
                 if (!localStorage.getItem('authtoken')) {
--- Project1/js/getB.js
+++ Project1/js/getB.js
@@ -1,48 +1,43 @@
-
-const subContainer=document.getElementById("sub");
-const blogs=document.getElementById("BLOGS");
+const subContainer = document.getElementById("sub");
+const blogs = document.getElementById("BLOGS");
+const loader = document.getElementById("loader")
 fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs')
-.then((result) => {
-    return result.json();
-})
-.then((result)=>{
-   
-    // Attaching to the blog slot on landing page
-    const lastThreePosts=result.data.slice(-3);
-    const allPost=result.data;
+    .then((result) => {
+        return result.json();
+    })
+    .then((result) => {
 
-lastThreePosts.forEach(post => {
+        // Attaching to the blog slot on landing page
+        const lastThreePosts = result.data.slice(-3);
+        const allPost = result.data;
 
-    const row1=document.createElement("div");
-    row1.classList.add("box","bg-light");
-   
+        lastThreePosts.forEach(post => {
 
-    const imagesource=document.createElement("img");
-    imagesource.classList.add("pictures");
-    imagesource.src=post.imageUrl;
-
-    const titleElement = document.createElement("h3");
-    titleElement.textContent = post.title;
-
-  const postAuthor = document.createElement("h5");
-  const contentElement = document.createElement("p");
-  contentElement.classList.add("light-text","py-1");
-  contentElement.textContent = post.message;
-  
-  row1.appendChild(imagesource);
-  row1.appendChild(titleElement);
-  row1.appendChild(postAuthor);
-  row1.appendChild(contentElement);
-  blogs.appendChild(row1); // append the post element to the container
-});
-
-})
-.catch((error)=>{
-    alert(error)
-})
+            const row1 = document.createElement("div");
+            row1.classList.add("box", "bg-light");
 
 
+            const imagesource = document.createElement("img");
+            imagesource.classList.add("pictures");
+            imagesource.src = post.imageUrl;
 
+            const titleElement = document.createElement("h3");
+            titleElement.textContent = post.title;
 
+            const postAuthor = document.createElement("h5");
+            const contentElement = document.createElement("p");
+            contentElement.classList.add("light-text", "py-1");
+            contentElement.textContent = post.message;
 
+            row1.appendChild(imagesource);
+            row1.appendChild(titleElement);
+            row1.appendChild(postAuthor);
+            row1.appendChild(contentElement);
+            blogs.appendChild(row1); // append the post element to the container
+            loader.style.display = "none";
+        });
 
+    })
+    .catch((error) => {
+        alert(error)
+    })
