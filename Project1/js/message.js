b81ef76ea8e420f332ff4b9d17b9aef619fa6c13
bergad37 <bergad37@gmail.com>
2023-03-19 13:42:57 +0200

    Validated the input fieds

--- Project1/js/message.js
+++ Project1/js/message.js
@@ -1,41 +1,47 @@
+const alarm = document.getElementById("alarm");
+const box = document.getElementById("alert-container");
+const messageForm = document.getElementById("message-me");
+const alert2 = document.getElementById("alert2");
+const alert1 = document.getElementById("alert1");
 
-const alarm=document.getElementById("alarm");
-const box=document.getElementById("alert-container");
-
-const messageForm=document.getElementById("message-me");
-
-
-messageForm.addEventListener("submit",(e)=>{
-    e.preventDefault();//prevent form from the defualt behaviour  
-
-const name=document.getElementById("msg-name").value;
-const email=document.getElementById("msg-email").value;
-const message=document.getElementById("message").value;
-
-const data={
-    name:name,
-    email:email,
-    message:message
-}
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault(); //prevent form from the defualt behaviour  
 
 
-fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs',
-{
-method:"POST",
-headers:{
-    "Content-Type":"application/json"
-},
-body:JSON.stringify(data)
-})
-.then((response) => {
-   return response.json();
-})
-.then((data) => {
-    alarm.textContent=data.message;  
-        box.style.display="block";
-   
-})
-.catch((err)=>{
-    alert(err)
-})
+    const name = document.getElementById("msg-name").value;
+    const email = document.getElementById("msg-email").value;
+    const message = document.getElementById("message").value;
+
+    //Validation 
+    if (name.trim() === "" || name.length < 3) {
+        return alert1.style.display = "block";
+    }
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+        return alert2.style.display = "block";
+    }
+    const data = {
+        name: name,
+        email: email,
+        message: message
+    }
+
+
+    fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            alarm.textContent = data.message;
+            box.style.display = "block";
+
+        })
+        .catch((err) => {
+            alert(err)
+        })
 })
