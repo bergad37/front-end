b859b8676a2426f9a0ff46ea71dac9757b9079d7
bergad37 <bergad37@gmail.com>
2023-03-19 12:00:18 +0200

    Edited the content length on the blog section

--- Project1/js/getB.js
+++ Project1/js/getB.js
@@ -22,12 +22,35 @@ fetch('https://tiny-puce-stingray-sock.cyclic.app/api/v1/blogs')
             imagesource.src = post.imageUrl;
 
             const titleElement = document.createElement("h3");
-            titleElement.textContent = post.title;
+            // titleElement.textContent = post.title;
+            titleElement.innerHTML = `<a href="/Project1/Pages/subpages/blogpanel.html">${post.title}</a>`
+
+
 
             const postAuthor = document.createElement("h5");
+            postAuthor.classList.add("author");
+            postAuthor.textContent = 'Written by' + post.Author;
             const contentElement = document.createElement("p");
             contentElement.classList.add("light-text", "py-1");
-            contentElement.textContent = post.message;
+            const maxLength = 200; // Set the maximum length of the content in characters
+            if (post.message.length > maxLength) {
+                // If the length of the post message is greater than the maximum length, truncate the content and add a "read more" link
+                const truncatedContent = post.message.substring(0, maxLength) + '...';
+                contentElement.innerHTML = truncatedContent + '<a href="#" class="read-more">Read More</a>';
+                const readMoreLink = contentElement.querySelector('.read-more');
+                readMoreLink.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    contentElement.innerHTML = post.message + '<a href="#" class="read-less">Read Less</a>';
+                    const readLessLink = contentElement.querySelector('.read-less');
+                    readLessLink.addEventListener('click', (event) => {
+                        event.preventDefault();
+                        contentElement.innerHTML = truncatedContent + '<a href="#" class="read-more">Read More</a>';
+                    });
+                });
+            } else {
+                contentElement.textContent = post.message;
+            }
+
 
             row1.appendChild(imagesource);
             row1.appendChild(titleElement);
